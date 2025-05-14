2846b41053e03fb3aec9d54f96b02f018fcb6b2a
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-05-14 01:52:31 -0400

    search bar features added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// Getting the search input
+const searchInput = document.getElementById("search-input");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+// Adding an event listener that listens to whenever the user types something into the search bar
+searchInput.addEventListener("input", function (e) {
+  // Get the value of the input
+  const input = e.target.value;
+  console.log(input);
+})
